1481b0a06a2819a9c9b3bbcf1eb6ccfcae2a7225
Connor Steel <csteel217@gmail.com>
2026-03-31 09:02:04 +1300

    updated packages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,22 +1,38 @@
 vim.opt.clipboard = "unnamedplus"
 
 vim.pack.add({
-	"https://github.com/stevearc/conform.nvim",
-	"https://github.com/nvim-lualine/lualine.nvim",
-	"https://github.com/neovim/nvim-lspconfig",
 	"https://github.com/nvim-mini/mini.nvim",
+	"https://github.com/folke/trouble.nvim",
+	"https://github.com/nvim-lualine/lualine.nvim",
+	"https://github.com/lewis6991/gitsigns.nvim",
+	"https://github.com/stevearc/conform.nvim",
+	"https://github.com/neovim/nvim-lspconfig",
 })
 
-vim.cmd.colorscheme("minicyan")
+vim.cmd.colorscheme("miniwinter")
+
 require("mini.basics").setup()
+
 require("mini.completion").setup()
+
 require("mini.files").setup()
+vim.keymap.set("n", "<leader>fm", ":lua MiniFiles.open()<cr>")
+
 require("mini.icons").setup()
+
 require("mini.pairs").setup()
+
 require("mini.snippets").setup()
+
 require("mini.surround").setup()
 
+require("trouble").setup()
+vim.keymap.set("n", "<leader>xx", ":Trouble diagnostics toggle<cr>")
+vim.keymap.set("n", "<leader>xq", ":Trouble qflist toggle<cr>")
+
 require("lualine").setup()
+
+require("gitsigns").setup()
 
 require("conform").setup({
 	formatters_by_ft = {
