b6798b63513fb967ec3c0a74b5b276adbd693d0e
Connor Steel <csteel217@gmail.com>
2026-03-31 16:39:22 +1300

    update colourscheme, configure lualine and fix conform formatters

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,9 +9,13 @@ vim.pack.add({
 	"https://github.com/neovim/nvim-lspconfig",
 })
 
-vim.cmd.colorscheme("miniwinter")
-
 require("mini.basics").setup()
+
+require("mini.hues").setup({
+	background = "#0f1416",
+	foreground = "#d0d4d5",
+	saturation = "lowmedium",
+})
 
 require("mini.completion").setup()
 
@@ -30,18 +34,61 @@ require("trouble").setup()
 vim.keymap.set("n", "<leader>xx", ":Trouble diagnostics toggle<cr>")
 vim.keymap.set("n", "<leader>xq", ":Trouble qflist toggle<cr>")
 
-require("lualine").setup()
+require("lualine").setup({
+	options = {
+		component_separators = "",
+		section_separators = "",
+	},
+	sections = {
+		lualine_a = {
+			{
+				"branch",
+				icon = "",
+				color = { gui = "bold" },
+			},
+		},
+		lualine_b = {
+			{
+				"diff",
+				symbols = { added = " ", modified = " ", removed = " " },
+				diff_color = {
+					added = { fg = "#98c379" },
+					modified = { fg = "#e5c07b" },
+					removed = { fg = "#e06c75" },
+				},
+			},
+		},
+		lualine_c = {
+			{
+				"diagnostics",
+				sources = { "nvim_diagnostic" },
+			},
+		},
+		lualine_x = { "location" },
+		lualine_y = { "encoding" },
+		lualine_z = { "filetype" },
+	},
+	inactive_sections = {
+		-- remove defaults
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {},
+	},
+})
 
 require("gitsigns").setup()
 
 require("conform").setup({
 	formatters_by_ft = {
-		lua = { "lua_ls" },
-		c = { "clangd" },
-		cpp = { "clangd" },
-		h = { "clangd" },
-		hpp = { "clangd" },
-		rs = { "rust_analyzer" },
+		lua = { "stylua" },
+		c = { "clang-format" },
+		cpp = { "clang-format" },
+		h = { "clang-format" },
+		hpp = { "clang-format" },
+		rs = { "rustfmt" },
 	},
 	format_on_save = {
 		timeout_ms = 750,
