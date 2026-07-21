ebd1e9af5e3cab6d8f723b609204dd73be1c68e4
Connor Steel <csteel217@gmail.com>
2026-07-21 14:14:12 +1200

    polish

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,5 +1,4 @@
 -- load config modules
 require("config.options")
 require("config.keymaps")
-require("config.diagnostics")
 require("config.lsp")
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,19 +1,2 @@
-local map = vim.keymap.set
-
--- diagnostics
-local diagnostic_goto = function(next, severity)
-	severity = severity and vim.diagnostic.severity[severity] or nil
-	return function()
-		vim.diagnostic.jump({ count = next and 1 or -1, float = true, severity = severity })
-	end
-end
-
-map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
-map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
-map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
-map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
-map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
-map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
-map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
-
-map("n", "<leader>qf", vim.diagnostic.setqflist, { desc = "Send Diagnostics to Quickfix" })
+vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+vim.keymap.set("n", "<leader>qf", vim.diagnostic.setqflist, { desc = "Send Diagnostics to Quickfix" })
