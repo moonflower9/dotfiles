cc276a46a7756bdc40e432d292f61316244d450d
Connor Steel <csteel217@gmail.com>
2026-07-23 12:47:04 +1200

    fix callback, remove redundant keymaps, more verbose

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,24 +1,15 @@
 vim.lsp.enable({ "lua_ls", "rust_analyzer", "clangd" })
+vim.lsp.inlay_hint.enable(true)
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
 	callback = function(event)
-		local bufnr = event.buf
-		local map = function(keys, fn, desc)
-			vim.keymap.set("n", keys, fn, { buffer = bufnr, desc = "LSP: " .. desc })
-		end
-
-		map("gd", vim.lsp.buf.definition, "Goto Definition")
-		map("gD", vim.lsp.buf.declaration, "Goto Declaration")
-		map("gi", vim.lsp.buf.implementation, "Goto Implementation")
-		map("gr", vim.lsp.buf.references, "References")
-		map("K", vim.lsp.buf.hover, "Hover Documentation")
-		map("<leader>rn", vim.lsp.buf.rename, "Rename")
-		map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
-		map("<leader>fmt", function()
-			require("conform").format({ async = true, lsp_format = "fallback" })
-		end, "Format")
-
-		vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf, desc = "LSP: Goto Definition" })
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = event.buf, desc = "LSP: Goto Declaration" })
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = event.buf, desc = "LSP: Goto Implementation" })
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = event.buf, desc = "LSP: References" })
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = event.buf, desc = "LSP: Hover Documentation" })
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = event.buf, desc = "LSP: Rename" })
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = event.buf, desc = "LSP: Code Action" })
 	end,
 })
