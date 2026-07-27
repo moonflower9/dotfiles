97aeb1d736218ced42f7e22b60da99e8e7153eb9
Connor Steel <csteel217@gmail.com>
2026-07-27 13:27:14 +1200

    remove indentscope

--- .config/nvim/after/plugin/mini.lua
+++ .config/nvim/after/plugin/mini.lua
@@ -2,7 +2,6 @@ require("mini.comment").setup()
 require("mini.diff").setup({ view = { style = "sign" } })
 require("mini.git").setup()
 require("mini.icons").setup()
-require("mini.indentscope").setup()
 require("mini.completion").setup()
 require("mini.surround").setup()
 require("mini.pick").setup()
@@ -11,24 +10,24 @@ vim.keymap.set("n", "<leader>vd", require("mini.diff").toggle_overlay, { desc = 
 
 local statusline = require("mini.statusline")
 local statusline_active = function()
-    local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
-    local git = statusline.section_git({ trunc_width = 75 })
-    local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
-    local lsp = statusline.section_lsp({ trunc_width = 75 })
-    local filename = statusline.section_filename({ trunc_width = 140 })
-    local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
-    local location = statusline.section_location({ trunc_width = 75 })
-    local search = statusline.section_searchcount({ trunc_width = 75 })
+	local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+	local git = statusline.section_git({ trunc_width = 75 })
+	local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
+	local lsp = statusline.section_lsp({ trunc_width = 75 })
+	local filename = statusline.section_filename({ trunc_width = 140 })
+	local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
+	local location = statusline.section_location({ trunc_width = 75 })
+	local search = statusline.section_searchcount({ trunc_width = 75 })
 
-    return statusline.combine_groups({
-        { hl = mode_hl,                 strings = { mode } },
-        { hl = "MiniStatuslineDevinfo", strings = { git, diagnostics, lsp } },
-        "%<",
-        { hl = "MiniStatuslineFilename", strings = { filename } },
-        "%=",
-        { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
-        { hl = mode_hl,                  strings = { search, location } },
-    })
+	return statusline.combine_groups({
+		{ hl = mode_hl, strings = { mode } },
+		{ hl = "MiniStatuslineDevinfo", strings = { git, diagnostics, lsp } },
+		"%<",
+		{ hl = "MiniStatuslineFilename", strings = { filename } },
+		"%=",
+		{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+		{ hl = mode_hl, strings = { search, location } },
+	})
 end
 statusline.setup({ content = { active = statusline_active } })
 
